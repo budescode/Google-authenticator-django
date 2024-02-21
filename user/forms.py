10a4f2c89be1a27df5e0aa8eaa52244eba789024
budescode <gospeltruth18@gmail.com>
2024-02-21 17:05:03 +0100

    authenticator added

---
+++ user/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import CustomUser
+from django.contrib.auth import authenticate
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ['fullname', 'email', 'username', 'password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise ValidationError('Passwords do not match')
+
+        return cleaned_data
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            print(username, password, user, "this is the user!")
+            if not user or not user.is_active:
+                raise forms.ValidationError('Invalid username or password.')
+            return cleaned_data
--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib import messages
 from helpers.generate_otp import extract_secret, generate_totp_qr_code, verify_otp
+from user.forms import LoginForm, RegistrationForm
 from .models import *
 from django.contrib.auth.models import User
 
@@ -12,80 +13,70 @@ def homeView(request):
     return render(request, 'index.html')
 
 def signupView(request):
-    referralcode = request.GET.get('referralcode', '')
-    if request.method == "POST": 
-        fullname = request.POST.get('fullname')
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password1 = request.POST.get("password1")
-        password2 = request.POST.get("password2")        
-        if User.objects.filter(email__iexact=email).exists():
-            messages.error(request, 'Email already taken')
-            return redirect('userurl:signup')
-        elif password1 != password2:
-            messages.error(request, 'Passwords do not match')
-            return redirect('userurl:signup')
-        else:
-            User.objects.create_user(username=email, password=password1,fullname=fullname,  email=email)
-
-            messages.success(request, 'Registration successful, verify your OTP')
-
-
-            
+    form = RegistrationForm() 
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        password = request.POST['password']
+        if form.is_valid():
+            user = form.save()  
+            user.set_password(password)  
+            user.save()    
+            messages.success(request, 'Registration successful')            
             return redirect('userurl:login')
-    return render(request, 'signup.html' , {'referralcode':referralcode})
+    return render(request, 'signup.html', {'form':form})
+
 
 def loginView(request):    
-    
-    if request.method == "POST": 
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-        try:
-            qs = User.objects.get(email__iexact=email)
-        except CustomUser.DoesNotExist:
-            messages.error(request, 'Invalid Credentials')
-            return render(request, 'signin.html')
-        
-        user = authenticate(request, username=qs.username, password=password)
-        
-        if user is not None:
-            secret = ''
-            secret_stored = False 
-            #used to know if the secret has been stored before or not so that we can display the qr code in frontend or not
+    form = LoginForm()
+    print("checking lignd.d.d.d.d")
+    if request.method == 'POST':
+        print("yes it is post!")
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            print(username, password, "printed..")
+            user = authenticate(username=username, password=password)
 
-            if user.authenticator_secret==None or user.authenticator_secret == '':                
-                qs = generate_totp_qr_code(user.email)
-                secret = extract_secret(qs)
-                
-            else:
-                secret_stored = True
+            if user is not None:
+                secret = ''
+                secret_stored = False 
+
+                #used to know if the secret has been stored before or not so that we can display the qr code in frontend or not
+
+                if user.authenticator_secret==None or user.authenticator_secret == '':                
+                    qs = generate_totp_qr_code(user.email)
+                    secret = extract_secret(qs)
+                    
+                else:
+                    secret_stored = True               
             
-           
-            return render(request, 'verify.html', {'qs':qs, 'email':email, 'secret_stored':secret_stored, 'secret':secret})
+                return render(request, 'verify.html', {'qs':qs, 'email':user.email, 'secret_stored':secret_stored, 'secret':secret})
 
-    return render(request, 'signin.html')
+    return render(request, 'signin.html', {'form':form})
+
+    
+
 
  
 def verifyOtp(request):
     if request.method == 'POST':
         otpcode = request.POST.get('otp')
-        email = request.POST.get('email')
-        
+        email = request.POST.get('email')        
         verify  = verify_otp(email, otpcode)
         if verify == False:
             messages.error(request, 'Invalid Code')
-            return redirect('userurl:login')
-
-
-                
+            return redirect('userurl:login')                
         user = User.objects.get(email__iexact=email)
-        login(request, user)     
+
+        login(request, user)   
+
         if user.authenticator_secret==None or user.authenticator_secret == '':                
             qs = generate_totp_qr_code(user.email)
             secret = extract_secret(qs)
             user.authenticator_secret = secret 
             user.save()
-           
+        messages.success(request, 'Authentication Successful')          
 
         return redirect('userurl:index')
         
